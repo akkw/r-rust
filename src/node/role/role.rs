2cedb608ceb1377f45c66ce590bb5561e78be82f
qiangzhiwei <318575094@qq.com>
2022-04-21 23:36:54 +0800

    first commit

---
+++ src/node/role/role.rs
@@ -0,0 +1,31 @@
+enum RoleName {
+    Leader,
+    Follower,
+    Candidate,
+}
+
+trait Role {
+
+}
+
+struct AbstractRole {
+    roleName: RoleName,
+    term: i32
+}
+
+impl Role for AbstractRole {
+
+}
+
+struct LeaderRole {
+   role: AbstractRole
+}
+
+struct FollowerRole {
+    role: AbstractRole
+}
+
+struct CandidateRole {
+    role: AbstractRole,
+    votesCount: i32,
+}
